80326eb5ca74ad9966679c94a9b4f153a3fdc52e
a-molaei <molaie.amir@gmail.com>
2019-05-29 20:17:59 +0430

    Setting

---
+++ SSO/Controllers/SettingController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SSO.UoW;
+using SSO.BLL;
+using SSO.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SSO.Controllers
+{
+    [Route("api/Setting")]
+    [ApiController]
+    public class SettingController : BaseApiController
+    {
+        public SettingController(IUnitOfWork unitOfWork, IUserManager userManager, IJwtHandler jwtHandler)
+            :base(unitOfWork, userManager, jwtHandler)
+        {
+
+        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var obj = UnitOfWork.SettingRepository.GetAll().FirstOrDefault();
+            var dto = new SettingDto();
+            if (obj != null)
+                dto.SaveTo(obj);
+                return Ok(dto);
+        }
+
+
+        [HttpPost]
+        [Route("Set")]
+        public IActionResult Set(SettingDto dto)
+        {
+            try
+            {
+                var obj = UnitOfWork.SettingRepository.GetAll().FirstOrDefault();
+                if (obj == null)
+                {
+                    var entity = dto.LoadFrom();
+                    UnitOfWork.SettingRepository.Add(entity);
+                }
+                else
+                {
+                    dto.CheckChanges(obj);
+                }
+                UnitOfWork.Complete();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
---
+++ SSO/ViewModels/Setting/SettingDto.cs
@@ -0,0 +1,45 @@
+﻿using SSO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SSO.ViewModels
+{
+    public class SettingDto
+    {
+        public int Id { get; set; }
+        public int MaxFailedPasswordCount { get; set; }
+        public int LockOutDuration { get; set; }
+        public int TokenExpirationDuration { get; set; }
+        public int PasswordExpirationDuration { get; set; }
+        public void SaveTo(Setting obj)
+        {
+            this.Id = obj.Id;
+            this.MaxFailedPasswordCount = obj.MaxFailedPasswordCount;
+            this.LockOutDuration = obj.LockOutDuration;
+            this.TokenExpirationDuration = obj.TokenExpirationDuration;
+            this.PasswordExpirationDuration = obj.PasswordExpirationDuration;
+        }
+        public Setting LoadFrom()
+        {
+            var obj = new Setting()
+            {
+                MaxFailedPasswordCount = this.MaxFailedPasswordCount,
+                LockOutDuration = this.LockOutDuration,
+                TokenExpirationDuration = this.TokenExpirationDuration,
+                PasswordExpirationDuration = this.PasswordExpirationDuration
+            };
+            return obj;
+        }
+        public Setting CheckChanges(Setting obj)
+        {
+
+            obj.MaxFailedPasswordCount = this.MaxFailedPasswordCount;
+            obj.LockOutDuration = this.LockOutDuration;
+            obj.TokenExpirationDuration = this.TokenExpirationDuration;
+            obj.PasswordExpirationDuration = this.PasswordExpirationDuration;
+            return obj;
+        }
+    }
+}
